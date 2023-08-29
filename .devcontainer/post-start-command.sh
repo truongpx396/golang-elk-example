cdeab741fea56f4c15abc654bde74841a2bbfe44
truongpx-Mac <truongpx396@gmail.com>
2023-08-29 21:08:53 +0700

    update

--- .devcontainer/post-start-command.sh
+++ .devcontainer/post-start-command.sh
@@ -1,4 +1,4 @@
 
-export KUBECONFIG=/usr/local/share/kube-localhost/config
+# export KUBECONFIG=$HOME/.kube/config
 kubectl port-forward svc/istio-ingressgateway 8080:80 -n istio-system &
 kubectl port-forward svc/kiali 20001:20001 -n istio-system &
